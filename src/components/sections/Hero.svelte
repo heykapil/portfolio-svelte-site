35d883c3b978b4e70e53c24e1a5a363fce8529d2
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-05-06 14:57:31 -0500

    Vertically center text on iOS; prevent weird resize behavior

--- src/components/sections/Hero.svelte
+++ src/components/sections/Hero.svelte
@@ -13,7 +13,14 @@
 		padding-top: 2rem;
 		padding-top: max(env(safe-area-inset-top), 2rem);
 
-		min-height: 100vh;
+		/*
+		Using load-vh (set in app.html) ensures that
+		text is centered on iOS,
+		and content doesn't move unexpectedly
+		when window is resizing
+		*/
+		min-height: calc(100 * var(--load-vh, 1vh));
+
 		display: flex;
 		flex-direction: column;
 
