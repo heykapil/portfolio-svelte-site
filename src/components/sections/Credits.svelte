74cd2837e157842d9ef34854dcb6a45c2f92f6e6
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-12-01 11:18:13 -0600

    Better credits

--- src/components/sections/Credits.svelte
+++ src/components/sections/Credits.svelte
@@ -5,20 +5,26 @@
 		<footer>
 			<cite>
 				Pablo Picasso,
-				<a target="_blank" href="https://www.uvu.edu/arts/applause/posts/stealing.html">Probably</a>
+				<a target="_blank" href="https://www.uvu.edu/arts/applause/posts/stealing.html">kinda</a>
 			</cite>
 		</footer>
 	</blockquote>
+	<p>
+		I <em>wish</em> I were creative enough to come up with all the cool things I did with this site!
+		Don't get me wrong: nothing here is copy/paste, and I tried to make it my own. But I wouldn't have
+		gotten here without some really great ideas, inspiration, and bug fixes from the following amazing
+		developers:
+	</p>
 	<dl>
 		<dt>Content Structure</dt>
 		<dd>
 			Thank You <a target="_blank" href="https://brittanychiang.com">Brittany Chiang</a>
 		</dd>
-		<dt>Profile SVG</dt>
+		<dt>Interactive Profile Pic</dt>
 		<dd>
 			Thank You <a target="_blank" href="https://www.cassie.codes">Cassie Evans</a>
 		</dd>
-		<dt>Bio Length</dt>
+		<dt>Bio Length Selection</dt>
 		<dd>
 			Thank You <a target="_blank" href="https://chriscoyier.net">Chris Coyier</a>
 		</dd>
